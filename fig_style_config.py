90e72d896de940ebe59ee9b73978df069c2e3078
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-09-21 18:55:49 +0200

    increase legend size

--- fig_style_config.py
+++ fig_style_config.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # Define your style parameters
 font_size = 12
-legend_font_size = 8
+legend_font_size = 10
 label_font_size = 12
 line_width = 1
 figure_size = (6, 4)
